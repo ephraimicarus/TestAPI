763f9c9f32983a928057fff6d6ffe12cbe67dc2f
Emilio Primic <eprimic123@outlook.com>
2024-07-12 13:01:14 +0200

    BaseApp recreation

---
+++ BaseApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BaseApp/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseApp.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Oib { get; set; } = string.Empty;
+    }
+}
